9561d6c4257d3a9090b1dad55b06008a1cd201ff
cholid ridwan <cholid@id.edge-asia.com>
2016-09-21 13:56:19 +0700

    change : button move when tap on it, and resize active after double tap

--- app/src/main/java/com/edgeasia/tes02/MainActivity.java
+++ app/src/main/java/com/edgeasia/tes02/MainActivity.java
@@ -1,9 +1,11 @@
 package com.edgeasia.tes02;
 
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private int ypos;
     private int width = 264;
     private int height = 144;
+    private boolean active = false;
+    private int click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +42,7 @@ public class MainActivity extends AppCompatActivity {
         canvas.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-                xpos = (int) event.getX();
-                ypos = (int) event.getY();
-
                 SGD.onTouchEvent(event);
-                if(!active) {
-                    btnTag.setX(xpos);
-                    btnTag.setY(ypos);
-                }
                 return true;
             }
         });
@@ -63,24 +59,52 @@ public class MainActivity extends AppCompatActivity {
             if(active) {
                 btnTag.setWidth((int) (width * scale));
                 btnTag.setHeight((int) (height * scale));
-                Log.i("scale", String.valueOf(btnTag.getWidth()) + ", " + String.valueOf(btnTag.getHeight()));
+                //Log.i("scale", String.valueOf(btnTag.getWidth()) + ", " + String.valueOf(btnTag.getHeight()));
             }
             return true;
         }
     }
 
-    private boolean active = false;
     private void tes() {
+        btnTag.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(!active){
+                    xpos = (int) event.getRawX();
+                    ypos = (int) event.getRawY();
+                    btnTag.setX(xpos - btnTag.getWidth()/2);
+                    btnTag.setY(ypos - btnTag.getHeight()*2);
+                }
+                return false;
+            }
+        });
+
         btnTag.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //active = (active == false) ? (active = true):(active = false);
-                if(!active){
-                    btnTag.setBackgroundColor(Color.GRAY);
-                    active = true;
-                }else {
-                    btnTag.setBackgroundResource(android.R.drawable.btn_default);
-                    active = false;
+                click++;
+                Handler handler = new Handler();
+                Runnable r = new Runnable() {
+
+                    @Override
+                    public void run() {
+                        click = 0;
+                    }
+                };
+
+                if (click == 1) {
+                    //Single click
+                    handler.postDelayed(r, 250);
+                } else if (click == 2) {
+                    //Double click
+                    click = 0;
+                    if(!active){
+                        btnTag.setBackgroundColor(Color.GRAY);
+                        active = true;
+                    }else {
+                        btnTag.setBackgroundResource(android.R.drawable.btn_default);
+                        active = false;
+                    }
                 }
             }
         });
